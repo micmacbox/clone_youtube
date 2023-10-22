68547a7e6cc54c142df74e6de855e4e80ec7c467
서주현 <beloved930223@gmail.com>
2023-10-23 01:07:09 +0900

    test: 비디오카드 스냅샷 테스트

--- src/components/tests/VideoCard.test.js
+++ src/components/tests/VideoCard.test.js
@@ -1,13 +1,37 @@
 import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
 import userEvent from "@testing-library/user-event";
 import { Route, useLocation } from "react-router-dom";
 import { formatAgo } from "../../util";
 import VideoCard from "../VideoCard";
 import { fakeVideo as video } from "../../test";
 import { withRouter } from "../../test/utils";
+import { VideoType } from "../../constants";
 
 describe("VideoCard", () => {
   const { title, channelTitle, publishedAt, thumbnails } = video.snippet;
+
+  it("renders default type correctly", () => {
+    const component = renderer.create(
+      withRouter(<Route path="/" element={<VideoCard video={video} />} />)
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it("renders relatedVideos type correctly", () => {
+    const component = renderer.create(
+      withRouter(
+        <Route
+          path="/"
+          element={<VideoCard video={video} type={VideoType.relatedVideos} />}
+        />
+      )
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
   it("renders video item", () => {
     render(
       withRouter(<Route path="/" element={<VideoCard video={video} />} />)
